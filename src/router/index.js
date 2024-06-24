ecb18c2774a84dc087fe59d3deb8175d34ad84e6
tianyuchen <tianyuchen@linkcm.com>
2024-06-24 12:46:10 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/yszc.vue'
+import yhxy from '../views/yhxy.vue'
 
 const router = createRouter({
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
@@ -17,11 +19,12 @@ const router = createRouter({
       path: '/yhxy',
       name: 'yhxy',
       title:'用户协议',
+      component: yhxy
 
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/yhxy.vue')
+      // component: () => import('../views/yhxy.vue')
     }
   ]
 })
