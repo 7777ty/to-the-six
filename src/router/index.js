62be3f395543f055201f9bd5a173b69960b9f5d7
tianyuchen <tianyuchen@linkcm.com>
2024-06-24 11:10:01 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -11,11 +11,14 @@ const router = createRouter({
     {
       path: '/yszc',
       name: 'yszc',
+      title:'隐私政策',
       component: HomeView
     },
     {
       path: '/yhxy',
       name: 'yhxy',
+      title:'用户协议',
+
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
